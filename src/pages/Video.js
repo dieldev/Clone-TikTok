883384a75362c96e3650431647c7da69038565d5
Sidiel Oliveira <sidiel.dev@gmail.com>
2023-07-04 20:47:57 -0300

    Player de vídeo com controle de pause/start

---
+++ src/pages/Video.js
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import "./video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
